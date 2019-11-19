ba7b305f808f34aab252a3c05a9fb49ebf93b274
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2019-11-19 15:01:48 +0300

    Update модуль1.py

--- модуль1.py
+++ модуль1.py
@@ -11,38 +11,10 @@ def nothing(x):
 mouse = Controller()
 sct = mss()
 
-def init_tr():
-    cv.namedWindow("Tracking1", cv.WINDOW_NORMAL)
-    cv.createTrackbar("num", "Tracking1", 4, 10, nothing)
-    cv.createTrackbar("val", "Tracking1", 20, 200, nothing)
-    cv.createTrackbar("dist", "Tracking1", 123, 200, nothing)
-    cv.createTrackbar("shift", "Tracking1", 0, 500, nothing)
-
-    cv.createTrackbar("from_width", "Tracking1", 303, 900, nothing)
-    cv.createTrackbar("to_width", "Tracking1", 580, 2560, nothing)
-    pass
-
-def get_tr(num):
-    val = cv.getTrackbarPos("val", "Tracking1")
-    shift = cv.getTrackbarPos("shift", "Tracking1")
-    dist = cv.getTrackbarPos("dist", "Tracking1")
-
-    top = [53 for i in range(num)]
-    for i in range(len(top)):
-        if i != 0:
-            top[i] += dist*i
-
-    height = [top[i]+val for i in range(num)]
-    left = cv.getTrackbarPos("from_width", "Tracking1")
-    width = cv.getTrackbarPos("to_width", "Tracking1")-left
-
-    return  top, height, left, width, shift
-
-
 def test1(nn):
     init_tr()
     num = cv.getTrackbarPos("num", "Tracking1")
-    top, height, left, width, shift = get_tr(num)
+    top, height, left, width, shift_y, shift_x = get_tr(num)
     cv.destroyAllWindows()
 
     arr = []
@@ -67,7 +39,7 @@ def test1(nn):
 def test2(nn):
     init_tr()
     num = cv.getTrackbarPos("num", "Tracking1")
-    top, height, left, width, shift = get_tr(num)
+    top, height, left, width, shift_y, shift_x = get_tr(num)
     cv.destroyAllWindows()
 
     arr = []
@@ -91,24 +63,72 @@ def test2(nn):
         arr.append(end)
     print(np.array(arr).mean())
 
+
+def init_tr():
+    cv.namedWindow("Tracking1", cv.WINDOW_NORMAL)
+    cv.createTrackbar("pos_y", "Tracking1", 53, 500, nothing)
+    cv.createTrackbar("pos_x", "Tracking1", 303, 500, nothing)
+
+    cv.createTrackbar("num_y", "Tracking1", 4, 10, nothing)
+    cv.createTrackbar("num_x", "Tracking1", 2, 10, nothing)
+
+    cv.createTrackbar("val_y", "Tracking1", 100, 500, nothing)
+    cv.createTrackbar("val_x", "Tracking1", 100, 500, nothing)
+
+    cv.createTrackbar("dist_y", "Tracking1", 123, 200, nothing)
+    cv.createTrackbar("dist_x", "Tracking1", 69, 200, nothing)
+
+    cv.createTrackbar("shift_y", "Tracking1", 0, 500, nothing)
+    cv.createTrackbar("shift_x", "Tracking1", 0, 500, nothing)
+    pass
+
+def get_tr(num_y, num_x):
+    pos_y = cv.getTrackbarPos("pos_y", "Tracking1")
+    val_y = cv.getTrackbarPos("val_y", "Tracking1")
+    shift_y = cv.getTrackbarPos("shift_y", "Tracking1")
+    dist_y = cv.getTrackbarPos("dist_y", "Tracking1")
+
+    pos_x = cv.getTrackbarPos("pos_x", "Tracking1")
+    val_x = cv.getTrackbarPos("val_x", "Tracking1")
+    shift_x = cv.getTrackbarPos("shift_x", "Tracking1")
+    dist_x = cv.getTrackbarPos("dist_x", "Tracking1")
+
+    top = [pos_y for i in range(num_y)]
+    for i in range(len(top)):
+        if i != 0:
+            top[i] += dist_y*i
+    height = [top[i]+val_y for i in range(num_y)]
+
+    left = [pos_x for i in range(num_x)]
+    for i in range(len(left)):
+        if i != 0:
+            left[i] += dist_x*i
+    width = [left[i]+val_x for i in range(num_x)]
+
+    return  top, height, left, width, shift_y, shift_x
+
 init_tr()
-num = cv.getTrackbarPos("num", "Tracking1")
-top, height, left, width, shift = get_tr(num)
+num_y = cv.getTrackbarPos("num_y", "Tracking1")
+num_x = cv.getTrackbarPos("num_x", "Tracking1")
+top, height, left, width, shift_y, shift_x = get_tr(num_y, num_x)
 
-while 0:
-    num = cv.getTrackbarPos("num", "Tracking1")
-    top, height, left, width, shift = get_tr(num)
+while 1:
+    num_y = cv.getTrackbarPos("num_y", "Tracking1")
+    num_x = cv.getTrackbarPos("num_x", "Tracking1")
+    top, height, left, width, shift_y, shift_x = get_tr(num_y, num_x)
 
-    img1 = np.array(sct.grab({'top': shift, 'left': left, 'width': width, 'height': height[-1]}))
-    #print(img1.shape)
-    for i in range(num):
-        img = img1[top[i]:height[i]]
-        #print(img.shape)
+    img1 = np.array(sct.grab({'top': shift_y, 'left': shift_x, 'width': width[-1], 'height': height[-1]}))
+    print(img1.shape)
+    for i in range(num_y):
+        for j in range(num_x):
+            img = img1[top[i]:height[i], left[j]:width[j]]
+            #print(img.shape)
 
-        cv.imshow(f"img{i}", img)
-        if cv.waitKey(1) & 0xFF == ord('2'):
-            cv.destroyAllWindows()
-            break
+            cv.imshow(f"{(i+1)+(j+1)*10}", img)
 
-test1(50)
-test2(50)
+    if cv.waitKey(1) & 0xFF == ord('2'):
+        cv.destroyAllWindows()
+        break
+
+#test1(50)
+#test2(50)
